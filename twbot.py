482121ffd7f88bb682dc72dafa1f98c871c6eb55
dmitryholovin <dmitryholovin.off@gmail.com>
2021-07-07 11:29:42 +0300

    addint start pr

---
+++ twbot.py
@@ -0,0 +1,6 @@
+# попробуй сначала так, а потом поменяй на False и прочитай ошибку что вылетит
+sasha_lubit_menya = True
+if sasha_lubit_menya:
+    print("ya lublu tebya")
+else:
+    raise ValueError(f"Sasha ne lubit menya:(")
